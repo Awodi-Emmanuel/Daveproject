15e96fde801e256da76a209ab88f8ed867b2eae7
NigerianCoder <gbemilanre@gmail.com>
2021-04-01 10:50:37 +0100

    Resolving heroku build failure

--- docsmart_django/docsmart_django/settings.py
+++ docsmart_django/docsmart_django/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -155,6 +156,5 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-import django_heroku
 
 django_heroku.settings(locals())
