2daf576637ad415e30ac396ff0e40394dd0b797b
NigerianCoder <gbemilanre@gmail.com>
2021-04-07 18:16:16 +0100

    Updating repo

--- docsmart_django/documents/classes/generator.py
+++ docsmart_django/documents/classes/generator.py
@@ -2,7 +2,10 @@ from .folders import get_user_files
 
 
 class Generator:
-    BASE_PATH = "/Users/DELL/Documents/Projects/storage/"  # os.environ.get('USER_PREFIX')
+    
+    # Macos Base Path: /var/www/html/storage/
+    # Windows Base Path: /Users/DELL/Documents/Projects/storage/
+    BASE_PATH = "/var/www/html/storage/"  # os.environ.get('BASE_PATH'),
     FOLDER_PREFIX = "user_"  # os.environ.get('BASE_PATH'),
 
     @staticmethod
