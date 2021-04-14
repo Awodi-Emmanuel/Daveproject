6fe8cf5e67586eb490cdf76f7cc1bcfd74641509
NigerianCoder <gbemilanre@gmail.com>
2021-04-14 18:22:57 +0100

    Vue Setup and configuration

---
+++ docsmart_vue/src/components/SignUp.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="signup">
+      <!-- Testing Testing -->
+    <form action="">
+        <input type="email">
+        <input type="password">
+        <button>Submit</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SignUp',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style>
+.margin-bottom {
+  margin-bottom: 15px;
+}
+
+.fade-enter,
+.fade-leave-active {
+  opacity: 0;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+</style>
