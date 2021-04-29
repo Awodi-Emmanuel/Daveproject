5b6ade24540b423cb830810c7611aca249cc535f
NigerianCoder <gbemilanre@gmail.com>
2021-04-29 09:30:57 +0100

    Update to notifications manager

--- docsmart_django/notifications/models.py
+++ docsmart_django/notifications/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from .manager import FAQManager
+from mdeditor.fields import MDTextField
+from user.models import User
 
-# Create your models here.
+
+class Notification(models.Model):
+
+    subject = models.CharField(verbose_name="Subject", max_length=500)
+    content = MDTextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name="User")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    REQUIRED_FIELDS = ['subject', 'category', 'content']
+
+    objects = FAQManager()
+
+    class Meta:
+        verbose_name = "Notification"
+        verbose_name_plural = "Notifications"
+
+    def __str__(self):
+        return str(self.subject)
+
+    def get_absolute_url(self):
+        return reverse("FAQ_detail", kwargs={"pk": self.pk})
