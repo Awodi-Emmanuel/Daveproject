7990ac6f2e73ebfb0ab5827023a0ae4dd2bb7e09
NigerianCoder <gbemilanre@gmail.com>
2021-05-05 09:55:40 +0100

    Added backends to base plugins

---
+++ docsmart_django/plugins_base/backends.py
@@ -0,0 +1,42 @@
+import jwt
+from rest_framework import authentication, exceptions
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.contrib.auth.backends import ModelBackend
+
+
+class PluginsAthorization(authentication.BaseAuthentication):
+
+    def authenticate(self, request):
+        auth_data = authentication.get_authorization_header(request)
+
+        if not auth_data:
+            return None
+
+        prefix, token = auth_data.decode('utf-8').split(' ')
+
+        try:
+            payload = jwt.decode(token, settings.JWT_SECRET_KEY, algorithms="HS256")
+            User = get_user_model()
+            user = User.objects.get(email=payload['email'])
+            return user, token
+
+        except jwt.DecodeError as identifier:
+            raise exceptions.AuthenticationFailed(
+                'Your token is invalid,login')
+        except jwt.ExpiredSignatureError as identifier:
+            raise exceptions.AuthenticationFailed(
+                'Your token is expired,login')
+
+
+class EmailBackend(ModelBackend):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        user_model = get_user_model()
+        try:
+            user = user_model.objects.get(email=username)
+        except user_model.DoesNotExist:
+            return None
+        else:
+            if user.check_password(password):
+                return user
+        return None
