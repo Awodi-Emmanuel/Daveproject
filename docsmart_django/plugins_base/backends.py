af2c2e00017ee710caaf2c5802954013b2cc42cb
NigerianCoder <gbemilanre@gmail.com>
2021-05-05 10:29:50 +0100

    Upadted backends authorization logic

--- docsmart_django/plugins_base/backends.py
+++ docsmart_django/plugins_base/backends.py
@@ -3,40 +3,26 @@ from rest_framework import authentication, exceptions
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.contrib.auth.backends import ModelBackend
+from company.models import Company
+from .models import Plugin, STATUS
 
 
 class PluginsAthorization(authentication.BaseAuthentication):
 
-    def authenticate(self, request):
-        auth_data = authentication.get_authorization_header(request)
+    def authenticate(self, request, user=None, plugin=None):
 
-        if not auth_data:
-            return None
-
-        prefix, token = auth_data.decode('utf-8').split(' ')
+        user = request.user
 
         try:
-            payload = jwt.decode(token, settings.JWT_SECRET_KEY, algorithms="HS256")
-            User = get_user_model()
-            user = User.objects.get(email=payload['email'])
-            return user, token
+            
+            company = Company.objects.get(user__id= user.id)
+            plugin = Plugin.objects.get(company = company.id, app=plugin)
 
-        except jwt.DecodeError as identifier:
+            if plugin.status == STATUS.ACTIVE:
+                
+                return plugin
+
             raise exceptions.AuthenticationFailed(
-                'Your token is invalid,login')
-        except jwt.ExpiredSignatureError as identifier:
-            raise exceptions.AuthenticationFailed(
-                'Your token is expired,login')
+                'You do not have access to this plugin')
 
 
-class EmailBackend(ModelBackend):
-    def authenticate(self, request, username=None, password=None, **kwargs):
-        user_model = get_user_model()
-        try:
-            user = user_model.objects.get(email=username)
-        except user_model.DoesNotExist:
-            return None
-        else:
-            if user.check_password(password):
-                return user
-        return None
